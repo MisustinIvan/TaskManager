0d7a5d0bc9fd910b98edd4d0b52af54905df5c4b
Ivan Misustin <ivanmis596@gmail.com>
2024-04-12 21:37:04 +0200

    project setup

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	cursor   int
+	choices  []string
+	selected map[int]struct{}
+}
+
+func initModel() model {
+	return nil
+}
+
+func main() {
+
+}
